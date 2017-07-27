b0f09eeeb4a70da91941ce57ea4a209e52c8fba6
lukyans <sukyans@gmail.com>
2017-07-27 10:56:44 -0600

    link controller creats links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,4 +2,16 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
   end
+
+  def create
+    binding.pry
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to root_path
+    end
+  end
+
+  def link_params
+    params.require(:link).permit(:url)
+  end
 end
