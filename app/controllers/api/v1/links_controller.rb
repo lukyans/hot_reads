0506a833cc87112b50045e0783fa38b71cf6f990
lukyans <sukyans@gmail.com>
2017-07-28 14:55:15 -0600

    pass ajax call

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -4,8 +4,8 @@ class Api::V1::LinksController < ApplicationController
   end
 
   def create
-    link = Link.find_or_create_by(url: params[:url]).increment!(:count,1)
-    @link = link.where(created_at: 24.hours.ago..Time.now)
-    render json: Link.all
+    @link = Link.find_or_create_by(url: params[:url]).increment!(:count,1)
+    @link.save
+    render json: Link.where(created_at: 24.hours.ago..Time.now)
   end
 end
