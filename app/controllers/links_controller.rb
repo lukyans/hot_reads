de9f822accf135cbfd64fa29d791ae0f74ad726c
lukyans <sukyans@gmail.com>
2017-07-31 18:09:08 -0600

    test endpoint have key count

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,10 +2,4 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
   end
-
-  # def create
-  #   link = Link.find_or_create_by(url: params[:url]).increment!(:count,1)
-  #   @link = link.where(created_at: 24.hours.ago..Time.now)
-  #   redirect_to root_path
-  # end
 end
--- spec/requests/api/v1/links_requests_spec.rb
+++ spec/requests/api/v1/links_requests_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe "Links API" do
   it "sends a list of urls" do
-    link1 = Link.create(url: "http://backend.turing.io/module4/")
-    link2 = Link.create(url: "https://www.uber.com/")
+    link1 = Link.create(url: "http://backend.turing.io/module4/", count: 1)
+    link2 = Link.create(url: "https://www.uber.com/", count: 4)
 
     get '/api/v1/links'
 
@@ -13,5 +13,6 @@ describe "Links API" do
 
     expect(links.count).to eq(2)
     expect(link).to have_key("url")
+    expect(link).to have_key("count")
   end
 end
